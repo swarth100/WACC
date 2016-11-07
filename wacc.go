bf50b1b1d31716210d785bc35582b4a1523632c6
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-11-07 15:30:44 +0000

    wacc.go: Incorporate flag library
    Signed-off-by: Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>

--- wacc.go
+++ wacc.go
@@ -3,32 +3,49 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
+	"time"
 )
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("%v FILE\n", os.Args[0])
-		os.Exit(1)
-	}
+const invalidArgError int = 1
+const fileReadError int = 2
+const syntacticError int = 100
+const symanticError int = 200
 
-	file, err := os.Open(os.Args[1])
+func errorHandler(err error, code int) {
+	fmt.Println(err)
+	os.Exit(code)
+}
+
+func main() {
+	var flags Flags
+
+	flags.Parse()
+
+	filename := flags.filename
+
+	file, err := os.Open(filename)
+
+	fmt.Printf("%v: %v\n", time.Now().Format("2006/01/02 15:04:05"), filename)
 
 	buffer, err := ioutil.ReadAll(file)
 	if err != nil {
-		log.Fatal(err)
+		errorHandler(err, fileReadError)
 	}
 
 	wacc := &WACC{Buffer: string(buffer)}
 	wacc.Init()
 
+	fmt.Println("-- Compiling...")
+
 	if err := wacc.Parse(); err != nil {
-		log.Print(err)
-		os.Exit(100)
+		errorHandler(err, syntacticError)
 	}
 
-	if len(os.Args) == 3 && os.Args[2] == "-t" {
+	if flags.printTree {
+		fmt.Println("-- Abstract Syntax Tree")
 		wacc.PrintSyntaxTree()
 	}
+
+	fmt.Println("\n-- Finished")
 }
