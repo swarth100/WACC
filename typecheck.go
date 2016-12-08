f8fb6e87b561f6c813bf49b289c17f5f910fbbc0
ss11915 <silvia.sapora15@imperial.ac.uk>
2016-12-08 16:52:21 +0000

    typecheck.go: Implement continue and break in dowhile and for loops

--- typecheck.go
+++ typecheck.go
@@ -798,7 +798,9 @@ func (m *DoWhileStatement) TypeCheck(ts *Scope, errch chan<- error) {
 		)
 	}
 
+	ts.loop = ts.loop + 1
 	m.body.TypeCheck(ts.Child(), errch)
+	ts.loop = ts.loop - 1
 
 	m.BaseStatement.TypeCheck(ts, errch)
 }
@@ -833,7 +835,9 @@ func (m *ForStatement) TypeCheck(ts *Scope, errch chan<- error) {
 			"Assign statement expected")
 	}
 
+	ts.loop = ts.loop + 1
 	m.body.TypeCheck(child, errch)
+	ts.loop = ts.loop - 1
 
 	m.BaseStatement.TypeCheck(ts, errch)
 }
